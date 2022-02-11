aa5b7adffa29f4fc5c58be3164508fae237672d8
Alex Sollman <alexndr.sollman@gmail.com>
2022-02-11 14:34:11 -0800

    Navigation and mobile updates.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* 
+// scroll position checker for old navigation to show/hide.
 let scrollPos = 0;
 const nav = document.querySelector('.navigation');
 
@@ -15,18 +17,27 @@ const checkPosition = () => {
   scrollPos = windowY;
 }
 
-window.addEventListener('scroll', checkPosition);
+window.addEventListener('scroll', checkPosition); */
 
-
+// too lazy to update every year, so just a basic age calc.
 const calcAge = () => {
   const currentYear = new Date().getFullYear();
-  const ageElem = document.querySelector('.current-age');
+  const ageElem = document.querySelector(".current-age");
 
-  
   ageElem.innerHTML += currentYear - 1989;
-
-}
+};
 
 calcAge();
 
-document.querySelector('.current-year').innerHTML += new Date().getFullYear();
+// places full year into the footer.
+document.querySelector(".current-year").innerHTML += new Date().getFullYear();
+
+// toggles the navigation menu when you click one of the links.
+const navCheckbox = document.querySelector(".mobile-nav__checkbox");
+const navLinks = document.querySelectorAll(".mobile-nav__link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navCheckbox.checked = false;
+  });
+});
