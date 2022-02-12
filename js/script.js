4cac2b31f3ae54d2906149279351e78708a3633e
Alex Sollman <alexndr.sollman@gmail.com>
2022-02-11 23:33:11 -0800

    code cleanup and CSS compression.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-/* 
-// scroll position checker for old navigation to show/hide.
-let scrollPos = 0;
-const nav = document.querySelector('.navigation');
-
-const checkPosition = () => {
-  let windowY = window.scrollY;
-  if (windowY < scrollPos) {
-    // Scrolling UP
-    nav.classList.add('is-visible');
-    nav.classList.remove('is-hidden');
-  } else {
-    // Scrolling DOWN
-    nav.classList.add('is-hidden');
-    nav.classList.remove('is-visible');
-  }
-  scrollPos = windowY;
-}
-
-window.addEventListener('scroll', checkPosition); */
-
 // too lazy to update every year, so just a basic age calc.
 const calcAge = () => {
   const currentYear = new Date().getFullYear();
